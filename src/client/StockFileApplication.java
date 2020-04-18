ea87b4d159c18469f29c669a4f499d251fd6155e
vitucal <vsitucal@up.edu.ph>
2020-04-18 23:13:47 +0800

    Cleaned this repo

--- src/client/StockFileApplication.java
+++ src/client/StockFileApplication.java
@@ -22,6 +22,7 @@ public class StockFileApplication {
 		List<HashMap<String, String>> dataResult = populateStockFileData(fr.getHeaders(), fr.readFileData());
 		StockFileData fileData = new StockFileData();
 		fileData.addData(dataResult);
+                System.out.println("Orders");
 		fileData.printData();
 		//System.out.println(dataResult.size());
                 
@@ -29,13 +30,14 @@ public class StockFileApplication {
 		List<HashMap<String, String>> dataResult2 = populateStockFileData(fr2.getHeaders(), fr2.readFileData());
 		StockFileData fileData2 = new StockFileData();
 		fileData2.addData(dataResult2);
+                System.out.println("Stock Data");
 		fileData2.printData();
 		//System.out.println(dataResult2.size());
          
                 int size2=dataResult2.size();
                 int size1=dataResult.size();
         
-        //Lists for Symbol, lotSize, and lastPrice         
+                //Lists for Symbol, lotSize, and lastPrice from stockdata.csv         
         
                 LinkedList<String> symbols = new LinkedList<>();
                 for(int a=0;a<size2;a++){
@@ -57,8 +59,30 @@ public class StockFileApplication {
                     lastPrice.add(data2);
                 }
                 //System.out.println(lastPrice);
+                //Lists for Symbol, Price, and Quantity from orders.csv
+                
+                LinkedList<Double> orderPrice = new LinkedList<>();
+                for(int a=0;a<size1;a++){
+                    double data2=Double.parseDouble(dataResult.get(a).get("Price"));
+                    orderPrice.add(data2);
+                }
+                //System.out.println(orderPrice);
+                
+                LinkedList<String> orderSymbols = new LinkedList<>();
+                for(int a=0;a<size1;a++){
+                    String data2 = dataResult.get(a).get("Symbol");
+                    orderSymbols.add(data2);
+                }
+                //System.out.println(orderSymbols);
+                
+                LinkedList<Double> orderQuantity = new LinkedList<>();
+                for(int a=0;a<size1;a++){
+                    double data2=Double.parseDouble(dataResult.get(a).get("Quantity"));
+                    orderQuantity.add(data2);
+                }
+                //System.out.println(orderQuantity);
               
-//Populate orderBook
+        //Populate orderBook
                 HashMap<String,List<Double>> acHash = new HashMap<>();
                 for(int n=0;n<symbols.size();n++){
                     //instanciate new acList to forget the old one
@@ -69,10 +93,10 @@ public class StockFileApplication {
                 }
                 //System.out.println(acHash.keySet());
                 
-                List<HashMap<String, String>> orderBook = new LinkedList<>();
-                for(int i=0;i<dataResult.size();++i){
+                LinkedList<HashMap<String, String>> orderBook = new LinkedList<>();
+                for(int i=0;i<dataResult.size();i++){
                 //System.out.println(dataResult.get(i).get("Symbol"));
-                    for(int i2=0;i2<dataResult2.size();++i2){
+                    for(int i2=0;i2<dataResult2.size();i2++){
                         //Instanciating a new LinkedList every time a for loop is reiterated
                         LinkedList<String> symbol = new LinkedList<>(acHash.keySet());
                         if(dataResult.get(i).get("Symbol").equals(symbol.get(i2))){
@@ -88,57 +112,124 @@ public class StockFileApplication {
                         }
                     }
                 }
-                System.out.println(orderBook);
                 
+        System.out.println("Order Book");
+        System.out.println(orderBook);
+        
         //execute trades on orderbook
-        HashSet<Integer> remove = new HashSet<>();
-        for(int o=0;o<orderBook.size();o++){
-            //System.out.println(orderBook.get(o));
-            for(int p=0;p<orderBook.size();p++){
-                if((!(orderBook.get(o).get("Side").equals(orderBook.get(p).get("Side"))))&&orderBook.get(o).get("Symbol").equals(orderBook.get(p).get("Symbol"))&&orderBook.get(o).get("Price").equals(orderBook.get(p).get("Price"))){
+        
+        //Buy Offers in order
+        LinkedList<HashMap<String, String>> buyOffer = new LinkedList<>();
+        for(int buyCount=0;buyCount<orderBook.size();buyCount++){
+            
+            if(orderBook.get(buyCount).get("Side").equals("BUY")){
+                buyOffer.add(orderBook.get(buyCount));
+            }
+        }
+        //System.out.println("Buy Offers:");
+        //System.out.println(buyOffer);
+        
+        //Sell Offers in order
+        LinkedList<HashMap<String, String>> sellOffer = new LinkedList<>();
+        for(int buyCount=0;buyCount<orderBook.size();buyCount++){
+            
+            if(orderBook.get(buyCount).get("Side").equals("SELL")){
+                sellOffer.add(orderBook.get(buyCount));
+            }
+        }
+        //System.out.println("Sell Offers:");
+        //System.out.println(sellOffer);
+            System.out.println(orderBook.size());
+        //while(true){
+            for(int loop=0;loop<orderBook.size();loop++){
+                for(int loop1=0;loop1<orderBook.size();loop1++){
+                    if(orderBook.get(loop).get("Symbol").equals(orderBook.get(loop1).get("Symbol"))){
+                        if(orderBook.get(loop).get("Side").equals("BUY")){
+                            if(orderBook.get(loop1).get("Side").equals("SELL")){
+                                double qty1 = Double.parseDouble(orderBook.get(loop).get("Quantity"));
+                                double qty2 = Double.parseDouble(orderBook.get(loop1).get("Quantity"));
+                                
+                                double buy = Double.parseDouble(orderBook.get(loop).get("Price"));
+                                double sell = Double.parseDouble(orderBook.get(loop1).get("Price"));
+                                
+                                if(buy==sell){
+                                    System.out.println("Current:");
+                                    System.out.println(orderBook);
+                                    System.out.println("Executing trade of"+orderBook.get(loop)+" and "+orderBook.get(loop1));
+                                    if(qty1>=qty2){
+                                        double n = qty1-qty2;
+                                        orderBook.get(loop).put("Quantity", Double.toString(n));
+                                        orderBook.get(loop1).put("Quantity", Double.toString(0));
+                                        //orderBook.remove(loop1);
+                                        if(n==0){
+                                        //orderBook.remove(loop);
+                                        }
+                                    }
                     
-                    double oQ = Double.parseDouble(orderBook.get(o).get("Quantity"));
-                    double pQ = Double.parseDouble(orderBook.get(p).get("Quantity"));
-                    
-                    if(pQ>=oQ){
-                        double n = pQ - oQ;
-                        orderBook.get(p).put("Quantity", Double.toString(n));
-                        remove.add(o);
-                        if(n==0){
-                        remove.add(p);
+                                    if(qty2>=qty1){
+                                        double m = qty2-qty1;
+                                        orderBook.get(loop1).put("Quantity", Double.toString(m));
+                                        orderBook.get(loop).put("Quantity", Double.toString(0));
+                                        //orderBook.remove(loop);
+                                        if(m==0){
+                                        //orderBook.remove(loop1);
+                                        }
+                                    }
+                                    System.out.println("After:");
+                                    System.out.println(orderBook);
+                                
+                                } 
+                            } 
                         }
-                    }
+ 
                     
-                    if(oQ>=pQ){
-                        double m = oQ - pQ;
-                        orderBook.get(o).put("Quantity", Double.toString(m));
-                        remove.add(p);
-                        if(m==0){
-                        remove.add(o);
-                        }
+                     //if(BUY)
                     }
                 }
             }
-        }
-        
-
-        //Iterator<Integer> remove2 = remove.iterator();
-        
-        //while(remove2.hasNext()){
-        //orderBook.remove(remove2.next());
+            //System.out.println(orderBook);
         //}
         
-        System.out.println(orderBook);
         
+        /*double buy = Double.parseDouble(buyOffer.get(0).get("Price"));
+        int qty1 = Integer.parseInt(buyOffer.get(0).get("Quantity"));
+        for(int loop=0;loop<sellOffer.size();loop++){
+            double sell = Double.parseDouble(sellOffer.get(loop).get("Price"));
+            int qty2 = Integer.parseInt(sellOffer.get(loop).get("Quantity"));
+            if(buyOffer.get(0).get("Symbol").equals(sellOffer.get(loop).get("Symbol"))){
                 
-	}
-	/**
-	 * Complete the method body so that it returns the given structure needed to 
-	 * populate the data field in the StockFileData class. 
-	 * @param headers
-	 * @param lines
-	 * @return List
-	 */
+                if(buy==sell){
+                    
+                    System.out.println("Executing trade of"+buyOffer.get(0)+" and "+sellOffer.get(loop));
+                    if(qty1>qty2){
+                        double n = qty1-qty2;
+                        buyOffer.get(0).put("Quantity", Double.toString(n));
+                        sellOffer.remove(loop);
+                        if(n==0){
+                        buyOffer.remove(0);
+                        }
+                    }
+                    
+                    if(qty2>qty1){
+                        double m = qty2-qty1;
+                        sellOffer.get(loop).put("Quantity", Double.toString(m));
+                        buyOffer.remove(0);
+                        if(m==0){
+                        sellOffer.remove(loop);
+                        }
+                    }
+                    
+                }
+            
+            }      
+        }
+            System.out.println(sellOffer);*/
+            
+        
+        
+	
+        }
+	
 	public static List<HashMap<String, String>> populateStockFileData(List<String> headers, List<String> lines){
 		List<HashMap<String, String>> dataResult = new ArrayList<>();
 		// Insert your code here..
